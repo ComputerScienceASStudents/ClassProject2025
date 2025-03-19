e66d08254fe163e5e1135e9e0d288a5eef31824e
t.macdonald <t.macdonald@28b5cs18.sjsd.lcl>
2025-03-19 10:47:12 -0500

    Test2

--- src/code/SearchingAndSorting.java
+++ src/code/SearchingAndSorting.java
@@ -1,6 +1,8 @@
 /** Required package class namespace */
 package code;
 
+import java.util.ArrayList;
+
 /**
  * SearchingAndSorting - description
  *
@@ -10,17 +12,36 @@ package code;
 public class SearchingAndSorting {
 
     private String names[];
+    private ArrayList<String> nameList;
 
     /**
      * Default constructor, sets class properties
      */
     public SearchingAndSorting() {
-        unsortNamesArray();
-        System.out.println(findName("Norman"));
-        System.out.println(findNamePosition("AJ"));
+        nameList = new ArrayList();
+        
+        unsortNames();
+//        System.out.println(findName("Norman"));
+//        System.out.println(findNamePosition("AJ"));
+        System.out.println(findNameInList("Rowan"));
+        System.out.println(findNamePositionInList("Teacher Man"));
+        int[] ages = {
+            5,
+            2,
+            7,
+            10,
+            0
+        };
+        System.out.println("ages: ");
+        outputInts(ages);
+        
+        int[] sortedAges = sillySort(ages);
+        
+        System.out.println("sortedAges: ");
+        outputInts(sortedAges);
     }
 
-    public void unsortNamesArray() {
+    public void unsortNames() {
         String names[] = {
             "Teddy",
             "AJ",
@@ -32,6 +53,12 @@ public class SearchingAndSorting {
             "Florence",
             "Nate"
         };
+        
+        nameList.clear();
+        
+        for (String name : names) {
+            nameList.add(name);
+        }
         
         this.names = names;
     }
@@ -50,18 +77,66 @@ public class SearchingAndSorting {
         return -1;
     }
     
+    public boolean findNameInList(String targetName) {
+        for (String name : nameList) {
+            if (name == targetName) return true;
+        }
+        
+        return true;
+    }
+    
+    public int findNamePositionInList(String targetName) {
+        for (int i = 0; i < nameList.size(); i++) {
+            if (nameList.get(i) == targetName) return i;
+        }
+        
+        return -1;
+    }
+    
     public void outputNames() {
         for (String name : names) {
             System.out.println("Name - " + name);
         }
     }
-    
-    public void sortNamesByLength() {
-        boolean sorted = false;
-        
-        while (!sorted) {            
-            
+     public void outputInts(int[] ints) {
+        for (int interger : ints) {
+            System.out.println("int - " + interger);
         }
+    }
+    
+    /**
+     * The silly ass sort function I made while bored
+     * 
+     * @param orgin
+     * @return 
+     */
+    public int[] sillySort(int[] orgin) {
+        int[] sortedArray = orgin;
+        
+        for (int i = 0; i < orgin.length; i++) {
+            int lastIndex = i - 1;
+            int curent = orgin[i];
+            if (lastIndex > -1) {
+                int last = orgin[lastIndex];
+                
+                if (curent > last) {
+                    System.out.println("int[" + i + "] (" + curent + ") is greater than int[" + lastIndex + "] (" + last + ")");
+                    System.out.println("[" + lastIndex + "] is now " + curent);
+                    sortedArray[lastIndex] = curent;
+                    System.out.println("[" + i + "] is now " + last);
+                    sortedArray[i] = last;
+                    
+                    System.out.println("New array step: ");
+                    outputInts(sortedArray);
+                    
+                    return sillySort(sortedArray);
+                }
+            } else {
+                System.out.println("int[" + i + "] (" + curent + ") This was the first int the array");
+            }
+        }
+        
+        return sortedArray;
     }
 
 }
